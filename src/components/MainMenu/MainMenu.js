c373e6392fd961d8e940d029e9f47063c448fd1f
JavontaeH <hardeman.javontae@gmail.com>
2022-04-25 15:56:00 -0500

    adjusting css and html for menu buttons, correct alignment wrong sizes atm

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,5 +1,10 @@
 import React from "react";
 import imageBg from "../../images/menu/townBg-effects.png";
+import playIcon from "../../images/menu/playPic.png";
+import boxIcon from "../../images/menu/boxIcon.png";
+import logoutIcon from "../../images/menu/logoutIcon.png";
+import loginIcon from "../../images/menu/loginIcon.png";
+import registerIcon from "../../images/menu/registerIcon.png";
 import "./MainMenu.css";
 
 export const MainMenu = ({ isAuthenticated, setAuthUser }) => {
@@ -10,16 +15,33 @@ export const MainMenu = ({ isAuthenticated, setAuthUser }) => {
           <div className="button-wrapper">
             <div className="button-wrapper-content">
               <h1>Battle Monsters</h1>
-              <div className="menu-buttons">
-                <div className="play-button">
-                  <button>Play</button>
+              <div className="top-menu-buttons">
+                <div className="menu-button">
+                  <div className="button-img-container">
+                    <img
+                      className="button-icon"
+                      src={playIcon}
+                      alt="Play Icon"
+                    />
+                  </div>
+                  Play
                 </div>
-                <div className="logout-button">
-                  <button>Logout</button>
+                <div className="menu-button">
+                  <div className="button-img-container">
+                    <img
+                      className="button-icon"
+                      src={logoutIcon}
+                      alt="Box Icon"
+                    />
+                  </div>
+                  Logout
                 </div>
-                <div className="box-button">
-                  <button>Box</button>
+              </div>
+              <div className="menu-button">
+                <div className="button-img-container">
+                  <img className="button-icon" src={boxIcon} alt="Box Icon" />
                 </div>
+                Box
               </div>
             </div>
           </div>
