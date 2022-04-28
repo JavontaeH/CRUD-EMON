9a662a2264a8e3c77b22f08479a8f59b45c41d6d
JavontaeH <hardeman.javontae@gmail.com>
2022-04-28 14:23:46 -0500

    changed some css trying to decide to use canvas or not for game

--- src/components/battle/Battle.js
+++ src/components/battle/Battle.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "./Battle.css";
 
-export const Battle = () => {
-  return <h1>hi</h1>;
+export const Battle = (props) => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const c = canvas.getContext("2d");
+    canvas.width = 1920;
+    canvas.height = 1080;
+
+    // background image
+    const bgImage = new Image();
+    bgImage.src = "images/battle/battleBackground.png";
+    bgImage.onload = () => {
+      c.drawImage(bgImage, 0, 0);
+    };
+
+    const pikaImg = new Image();
+    pikaImg.src =
+      "https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif";
+
+    // animation loop
+    function animateBattle() {
+      c.drawImage(pikaImg, 1500, 200, pikaImg.width * 3, pikaImg.height * 3);
+      window.requestAnimationFrame(animateBattle);
+    }
+    animateBattle();
+  }, []);
+
+  return <canvas ref={canvasRef} {...props} />;
 };
