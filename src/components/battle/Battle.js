e1464bed2ec85ad1de83d517d3d614161e5ed3c7
JavontaeH <hardeman.javontae@gmail.com>
2022-04-28 15:57:37 -0500

    changed to no canvas

--- src/components/battle/Battle.js
+++ src/components/battle/Battle.js
@@ -2,40 +2,27 @@ import React, { useRef, useEffect } from "react";
 import "./Battle.css";
 
 export const Battle = (props) => {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const c = canvas.getContext("2d");
-    canvas.width = 1920;
-    canvas.height = 1080;
-
-    // background image
-    const bgImage = new Image();
-    bgImage.src = "images/battle/battleBackground.png";
-    bgImage.onload = () => {
-      c.drawImage(bgImage, 0, 0);
-    };
-
-    const pikaImg = new Image();
-    pikaImg.src =
-      "https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif";
-
-    const pikaBackImg = new Image();
-    pikaBackImg.src =
-      "https://img.pokemondb.net/sprites/black-white/anim/back-normal/pikachu.gif";
-
-    // animation loop
-    function animateBattle() {
-      c.drawImage(pikaImg, 1500, 200, pikaImg.width * 3, pikaImg.height * 3);
-      c.drawImage(pikaBackImg, 600, 600, pikaImg.width * 3, pikaImg.height * 3);
-      window.requestAnimationFrame(animateBattle);
-    }
-    animateBattle();
-  }, []);
+  const pokemon = {
+    name: "pokemon",
+    img: "https://img.pokemondb.net/sprites/black-white/anim/normal/pikachu.gif",
+  };
+  const pokemon2 = {
+    name: "pokemon",
+    img: "https://img.pokemondb.net/sprites/black-white/anim/back-normal/pikachu.gif",
+  };
 
   return (
     <div className="battle-wrapper">
+      <div className="user-pokemon-wrapper">
+        <div className="user-pokemon">
+          <img src={`${pokemon2.img}`} />
+        </div>
+      </div>
+      <div className="enemy-pokemon-wrapper">
+        <div className="enemy-pokemon">
+          <img src={`${pokemon.img}`} />
+        </div>
+      </div>
       <div className="attack-interface-wrapper">
         <div className="attack-options">
           <button>Attack 1</button>
@@ -47,7 +34,6 @@ export const Battle = (props) => {
           <h1>Attack Type</h1>
         </div>
       </div>
-      <canvas ref={canvasRef} />
     </div>
   );
 };
